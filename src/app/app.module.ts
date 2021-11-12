74bc2d27211cafa98ffc0872e8780418533021aa
Murat Can Batıbay <muratcanbatibay@gmail.com>
2021-11-12 19:29:07 +0300

    Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarComponent } from './components/car/car.component';
+import { CategoryComponent } from './components/category/category.component';
+import { NaviComponent } from './components/navi/navi.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarComponent,
+    CategoryComponent,
+    NaviComponent
   ],
   imports: [
     BrowserModule,
